94bab49cd30b2a4fd9ca476b92fc9258af204abb
Higor Correa <higor_manoel2@hotmail.com>
2022-01-15 14:47:31 -0300

    Added message date

--- webchat.api/WebChat.Api/Hubs/ChatHub.cs
+++ webchat.api/WebChat.Api/Hubs/ChatHub.cs
@@ -4,10 +4,10 @@ namespace WebChat.Api.Hubs;
 
 public class ChatHub : Hub
 {
-  
+
 
     public async Task SendMessage(string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", Context.User?.Identity?.Name, message);
+        await Clients.All.SendAsync("ReceiveMessage", Context.User?.Identity?.Name ?? "unknown", message, DateTime.UtcNow);
     }
 }
